70db49af156b6bdfbbaa4ed7a11cb3513a7dbb34
Alexander Panov <ya-khabara@yandex.ru>
2019-10-01 19:12:39 +1000

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.min + this.max) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max) / 2);
     }
 }
 
